1b4047e5a55d681d1edf8b7f88caf88a9a46190e
Devin Alexander <devinmalexander@gmail.com>
2025-01-24 14:32:49 -0600

    Changes to several files

---
+++ Player/Attack/javelin.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var level = 1
+var hp = 9999
+var speed = 200.0
+var damage = 10
+var knockback_amount = 100
+var paths = 1
+var attack_size = 1.0
+var attack_speed = 5.0
+
+var target = Vector2.ZERO
+var target_array = []
+
+var angle = Vector2.ZERO
+var reset_pos = Vector2.ZERO
+
+
+
+
+
+func _ready() -> void:
+	pass # Replace with function body.
